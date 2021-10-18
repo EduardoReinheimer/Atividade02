a2a45276fdceb2f45efd4e23ba601c87105b315f
Eduardo Salvador Reinheimer <du428@hotmail.com>
2021-10-17 21:25:15 -0300

    subindo par ou ímpar

--- menu.php
+++ menu.php
@@ -1,2 +1,3 @@
 <a href="dados.php">Dados</a>
+<a href="par_ou_impar.php">Par ou ímpar</a>
 <br><br>
---
+++ par_ou_impar.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<?php
+$numUser = isset($_POST["numUser"]) ? $_POST["numUser"] : 0;
+$numComputador = rand(0, 10);
+$resultado = $numComputador + $numUser;
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Par ou Ímpar</title>
+</head>
+
+<body>
+
+    <?php
+    include "menu.php";
+    ?>
+    <form action="" method="post">
+        Número: <input type="number" name="numUser" min="0" max="10" required>
+        <br>
+        <ul>
+            <li>
+                <input type="radio" name="modo" value="par" checked>
+                <label for="par">Par</label>
+            </li>
+            <li>
+                <input type="radio" name="modo" value="impar">
+                <label for="impar">Ímpar</label>
+            </li>
+        </ul>
+
+
+        <br>
+        <input type="submit" value="Jogar">
+    </form>
+    <?php
+    $radioModo = $_POST["modo"];
+    $isPar;
+    if ($radioModo == "par") {
+        $isPar = true;
+    } else if ($radioModo == "impar") {
+        $isPar = false;
+    }
+
+    $victory;
+    if ($isPar) {
+        $victory = $resultado % 2 == 0;
+    } else {
+        $victory = $resultado % 2 != 0;
+    }
+
+    echo "<h1>Número Usuário: $numUser</h1>";
+    echo "<h1>Número Computador: $numComputador</h1>";
+
+    if ($victory) {
+        echo "<h1>Vitória</h1>";
+    } else {
+        echo "<h1>Você perdeu</h1>";
+    }
+    ?>
+
+    <h1></h1>
+
+</body>
+
+</html>
